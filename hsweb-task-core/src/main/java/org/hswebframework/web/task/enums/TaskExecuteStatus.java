b6da64bdf6c33b2ba2d00dd908567fbb6cc81f04
ShawnShoper <xiehao,930825>
2018-08-27 15:03:00 +0800

    修改任务状态，starting-->preparing

--- hsweb-task-core/src/main/java/org/hswebframework/web/task/enums/TaskExecuteStatus.java
+++ hsweb-task-core/src/main/java/org/hswebframework/web/task/enums/TaskExecuteStatus.java
@@ -1,11 +1,18 @@
 package org.hswebframework.web.task.enums;
 
 public enum TaskExecuteStatus {
+    //执行成功
     success,
+    //执行失败
     failed,
+    //执行中
     running,
+    //挂起
     suspend,
+    //阻断
     interrupt,
+    //超时
     timeout,
-    starting;
+    //准备中
+    preparing;
 }
