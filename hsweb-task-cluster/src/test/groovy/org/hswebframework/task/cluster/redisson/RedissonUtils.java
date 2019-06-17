84b22104de180ce625bb9b641b868a8196fa4e69
zhou-hao <admin@hsweb.me>
2019-06-17 15:55:40 +0800

    修复测试错误

--- hsweb-task-cluster/src/test/groovy/org/hswebframework/task/cluster/redisson/RedissonUtils.java
+++ hsweb-task-cluster/src/test/groovy/org/hswebframework/task/cluster/redisson/RedissonUtils.java
@@ -13,7 +13,7 @@ public class RedissonUtils {
     public static RedissonClient createRedissonClient() {
         Config config = new Config();
         config.useSingleServer()
-                .setAddress(System.getProperty("hsweb.task.cluster.redis.address", "tcp://127.0.0.1:6379"))
+                .setAddress(System.getProperty("hsweb.task.cluster.redis.address", "redis://127.0.0.1:6379"))
                 .setDatabase(Integer.getInteger("hsweb.task.cluster.redis.database", 0));
         return Redisson.create(config);
     }
